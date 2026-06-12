4b6ed3dc5e76b6bf26639ca56d8e1273f7ffebd3
Luca Ciucci <luca.ciucci99@gmail.com>
2026-06-12 23:33:06 +0200

    wip

--- src/components/index.ts
+++ src/components/index.ts
@@ -13,3 +13,4 @@ export {
 } from "./project/index.ts";
 export { default as SupportMe } from "./SupportMe.astro";
 export { default as Timeline, type TimelineEntry } from "./Timeline.astro";
+export { default as MetaEx } from "./MetaEx.astro";
---
+++ src/layouts/index.ts
@@ -0,0 +1,3 @@
+export { default as BaseHtmlPage } from './BaseHtmlPage.astro';
+export { default as BasePage, type Props as BasePageProps, type PageMeta } from './BasePage.astro';
+export { default as MainLayout } from './MainLayout.astro';
